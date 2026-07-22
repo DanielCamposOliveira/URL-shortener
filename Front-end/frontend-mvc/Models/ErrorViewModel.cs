4346de532a45d656b2d72edbc479c1b73835add8
Daniel <daniel_cpd2017@hotmail.com>
2026-07-22 15:17:55 -0300

    chore: remove frontend-mvc da dev_front

--- Front-end/frontend-mvc/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace frontend_mvc.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
