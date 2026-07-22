d2b36b9a8661aef5df35128532cbc081c32805f5
Daniel <daniel_cpd2017@hotmail.com>
2026-07-22 15:38:48 -0300

    chore: remove back-end da dev_front

--- Back-end/API-Data/src/Models/OperationResultModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace API_Data.src.Models
-{
-    public class OperationResult
-    {
-        public bool Success { get; set; }
-        public string? Message { get; set; }
-    } 
-}
--- Back-end/API-Data/src/Utils/IJwtService.cs
+++
@@ -1,9 +0,0 @@
-﻿using API_Data.src.Models;
-
-namespace API_Data.src.Utils
-{
-    public interface IJwtService
-    {
-        string GenerateToken(User user);
-    }
-}
